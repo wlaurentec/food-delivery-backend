5ddf63fbe32226e5914e8d143405ed1e90aa4675
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-06 11:52:06 -0500

    add addFood controllers function

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,10 +1,25 @@
-import foodModel from "../models/foodModels";
+import foodModel from "../models/foodModels.js";
 import fs from "fs";
 
 // add food Item
 
 const addFood = async (req, res) => {
+  let image_filename = `${req.file.originalname}`;
 
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
 export { addFood };
