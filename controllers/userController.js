36acde50d0a960c8d9c27311b7c752845fffb2b1
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-09 09:44:38 -0500

    add /api/user/register

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,55 @@ import validator from "validator";
 // login user
 const loginUser = async (req, res) => {};
 
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
 // register user
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    // checking if user already exists
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    // validating email format & strong password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    const token = createToken(user._id);
+
+    res.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 export { loginUser, registerUser };
